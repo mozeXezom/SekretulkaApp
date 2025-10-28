e15c2bafbb4412a8e4bcaaee1b29a4d3e166c6d9
mozeX <delix.cito@gmail.com>
2025-10-28 20:04:23 +0200

    Initial Commit

---
+++ SekretulkaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SekretulkaApp
+//
+//  Created by Yurii Derzhylo on 28.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
